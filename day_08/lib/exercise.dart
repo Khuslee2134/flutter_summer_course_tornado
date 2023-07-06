cd5c5c4de0d409b7f28dbec481e07cfcc0cd8462
Tornado-1 <tornado-1@Admin-1s-iMac.local>
2023-07-06 17:34:01 +0800

    day_08.dart

---
+++ day_08/lib/exercise.dart
@@ -0,0 +1,18 @@
+void main() {
+  print('Day_08');
+  print(avarage(4, 3));
+  print(d(2.3, 3.2, 1.5));
+  q('Hi', 'HEllo');
+}
+
+double avarage(int a, int b) {
+  return (a + b) / 2;
+}
+
+double d(double e, double f, double j) {
+  return e * f * j;
+}
+
+void q(String w, String r) {
+  print('$w$r');
+}
