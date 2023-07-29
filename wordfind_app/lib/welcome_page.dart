2da7a15d2d57d29908baf96b74d1bfca512bd6c2
Tornado-1 <tornado-1@Admin-1s-iMac.local>
2023-07-29 17:49:59 +0800

    add:Word_find

--- wordfind_app/lib/welcome_page.dart
+++ wordfind_app/lib/welcome_page.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:wordfind_app/gradient_letter.dart';
 import 'package:wordfind_app/gradient_text.dart';
@@ -19,51 +17,33 @@ class WelcomePage extends StatelessWidget {
           ),
         ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Padding(
-              padding: EdgeInsets.only(top: 120),
-            ),
-            Container(
-              child: Expanded(
-                child: Column(children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      GradientLetter('W'),
-                      Padding(padding: EdgeInsets.only(left: 10),),
-                      GradientLetter('O'),
-                      Padding(padding: EdgeInsets.only(left: 10),),
-                      GradientLetter('R'),
-                      Padding(padding: EdgeInsets.only(left: 10),),
-                      GradientLetter('D'),
-                      Padding(padding: EdgeInsets.only(left: 10),),
-                    ],
-                  ),
-                  Expanded(
-                    child: GradientText(text: 'Game', size: 31.6),
-                  ),
-                ]),
+            Padding(padding: EdgeInsets.only(top: 120)),
+            Expanded(child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GradientLetter('W'),
+                    Padding(padding: EdgeInsets.only(left: 10,top: 120)),
+                    GradientLetter('O'),
+                    Padding(padding: EdgeInsets.only(left: 10)),
+                    GradientLetter('R'),
+                    Padding(padding: EdgeInsets.only(left: 10)),
+                    GradientLetter('D'),
+                    Padding(padding: EdgeInsets.only(left: 10)),
+                  ],
+                ),
+                GradientText(text: 'GAME', size: 31.6),
+                Padding(padding: EdgeInsets.only(top: 0)),
+              ],
+            )),
+            Expanded(child: Image.asset('assets/iCodeGuy.png', width: 374, height: 374)),
 
-              ),
-
-            ),
-            Expanded(
-              child: Image.asset(
-                'assets/iCodeGuy.png',
-                width: 374,
-                height: 374,
-              ),
-            ),
-            Expanded(
-              child: GradientText(
-                text: 'Ready ?',
-                size: 25,
-              ),
-            ),
+            Expanded(child: GradientText(text: 'READY', size: 25,)),
+            // Padding(padding: EdgeInsets.only(bottom: 22)),
           ],
         ),
-        margin: EdgeInsets.only(bottom: 193),
       ),
       floatingActionButton: Container(
         width: 310,
@@ -83,12 +63,13 @@ class WelcomePage extends StatelessWidget {
               elevation: 0,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(25),
+
               )),
           child: Text(
             'Play',
             style: TextStyle(
                 fontFamily: 'Nunito',
-                fontSize: 33,
+                fontSize: 24,
                 fontWeight: FontWeight.w700),
           ),
         ),
